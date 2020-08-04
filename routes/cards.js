1672e46da75c8584b129c8097fd96a328e6d74a4
sergeivaskin1991 <sergeivaskin@gmail.com>
2020-08-04 23:35:24 +0300

    second commit

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const cardsRouter = (req, res) => {
+  // eslint-disable-next-line global-require,import/no-dynamic-require
   const cards = require(path.join(__dirname, '../data/cards.json'));
   res.send(cards);
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const router = require('express').Router();
 
+// eslint-disable-next-line import/no-dynamic-require
 const users = require(path.join(__dirname, '../data/users.json'));
+const fs = require('fs').promises;
 
 const usersRouter = (req, res) => {
   res.send(users);
@@ -8,15 +11,25 @@ const usersRouter = (req, res) => {
 
 const userRouter = (req, res) => {
   const { id } = req.params;
-  const result = users.find(( { _id } ) => _id === id);
+  const result = users.find(({ _id }) => _id === id);
 
   if (!result) {
-    res.status(404).send({message: 'Нет пользователя с таким id'});
+    res.status(404).send({ message: 'Нет пользователя с таким id' });
     return;
   }
 
   res.send(result);
 };
+
+router.get('/', (req, res) => {
+  fs.readFile(users, 'utf8')
+    .then((routes) => {
+      res.send(routes);
+    })
+    .catch(() => {
+      res.status('500').send({ ERROR: 'Данные не найдены' });
+    });
+});
 
 module.exports = {
   usersRouter,
